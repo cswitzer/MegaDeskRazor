6769a2cb88773cab045754a75633406bc2c35791
Chase <chasehillig@yahoo.com>
2022-06-08 20:29:31 -0600

    ran into a bug

---
+++ MegaDesk/Migrations/20220607224610_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDesk.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeliveryType",
+                columns: table => new
+                {
+                    DeliveryTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeliveryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceUnder1000 = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PriceBetween1000And2000 = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PriceOver1000 = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryType", x => x.DeliveryTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DesktopMaterial",
+                columns: table => new
+                {
+                    DesktopMaterialId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DesktopMaterialName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DesktopMaterial", x => x.DesktopMaterialId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Desk",
+                columns: table => new
+                {
+                    DeskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Depth = table.Column<int>(type: "int", nullable: false),
+                    NumDrawers = table.Column<int>(type: "int", nullable: false),
+                    DesktopMaterialId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Desk", x => x.DeskId);
+                    table.ForeignKey(
+                        name: "FK_Desk_DesktopMaterial_DesktopMaterialId",
+                        column: x => x.DesktopMaterialId,
+                        principalTable: "DesktopMaterial",
+                        principalColumn: "DesktopMaterialId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeskQuote",
+                columns: table => new
+                {
+                    DeskQuoteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuoteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuotePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DeskId = table.Column<int>(type: "int", nullable: false),
+                    DeliveryTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeskQuote", x => x.DeskQuoteId);
+                    table.ForeignKey(
+                        name: "FK_DeskQuote_DeliveryType_DeliveryTypeId",
+                        column: x => x.DeliveryTypeId,
+                        principalTable: "DeliveryType",
+                        principalColumn: "DeliveryTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeskQuote_Desk_DeskId",
+                        column: x => x.DeskId,
+                        principalTable: "Desk",
+                        principalColumn: "DeskId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Desk_DesktopMaterialId",
+                table: "Desk",
+                column: "DesktopMaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeskQuote_DeliveryTypeId",
+                table: "DeskQuote",
+                column: "DeliveryTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeskQuote_DeskId",
+                table: "DeskQuote",
+                column: "DeskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeskQuote");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryType");
+
+            migrationBuilder.DropTable(
+                name: "Desk");
+
+            migrationBuilder.DropTable(
+                name: "DesktopMaterial");
+        }
+    }
+}
---
+++ MegaDesk/Pages/DeskQuotes/Create.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MegaDesk.Data;
+using MegaDesk.Models;
+using System.Diagnostics;
+
+namespace MegaDesk.Pages.DeskQuotes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MegaDesk.Data.MegaDeskContext _context;
+
+        public CreateModel(MegaDesk.Data.MegaDeskContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Desk Desk { get; set; }
+
+        [BindProperty]
+        public DeskQuote DeskQuote { get; set; }
+
+        public IActionResult OnGet()
+        {
+            ViewData["DeliveryTypeId"] = new SelectList(_context.Set<DeliveryType>(), "DeliveryTypeId", "DeliveryName");
+            ViewData["DesktopMaterialId"] = new SelectList(_context.Set<DesktopMaterial>(), "DesktopMaterialId", "DesktopMaterialName");
+            return Page();
+        }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // TODO: delete quote price = 0 later and call GetQuotePrice instead
+            DeskQuote.QuotePrice = 0;
+            DeskQuote.QuoteDate = DateTime.Now;
+
+            if (!ModelState.IsValid || _context.DeskQuote == null || DeskQuote == null)
+            {
+                Debug.WriteLine(DeskQuote.DeliveryType);
+                // Debug.WriteLine(DeskQuote.Desk.DesktopMaterial); Desk not set to instance of an object
+                return Page();
+            }
+
+          _context.DeskQuote.Add(DeskQuote);
+          await _context.SaveChangesAsync();
+
+          return RedirectToPage("./Index");
+        }
+    }
+}
